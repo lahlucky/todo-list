d97fbb8d389bfdc3f7b0a3760b1c53930c47af81
lucky lah <luckylahvander@gmail.com>
2023-01-29 19:42:49 -0800

    Updated TodoList and changed colors on CSS

--- src/App.js
+++ src/App.js
@@ -1,55 +1,27 @@
 import React, { useState, useRef, useEffect } from 'react'
 import "./App.css"
 import TodoList from './components/TodoList'
-import TodoForm from './components/TodoForm'
 
 // const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
-  const todoNameRef = useRef()
+  // const todoNameRef = useRef()
 
-  //   //to load the todos
-  //   useEffect(() => {
-  //     const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-  //     if (storedTodos) setTodos(previousTodos => [...previousTodos, ...storedTodos])
-  //   }, [])
+  // //to load the todos
+  // useEffect(() => {
+  //   const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+  //   if (storedTodos) setTodos(previousTodos => [...previousTodos, ...storedTodos])
+  // }, [])
 
-  //   //to save the todos
-  //   useEffect(() => {
-  //     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  //   }, [todos])
-
-  //   //toggles todos from complete to incomplete and vice-versa
-  //   function toggleTodo(id) {
-  //     const newTodos = [...todos]
-  //     const todo = newTodos.find(todo => todo.id === id)
-  //     todo.complete = !todo.complete
-  //     setTodos(newTodos)
-  //   }
-
-  //   function handleAddTodo(e) {
-  //     const name = todoNameRef.current.value
-  //     if (name === '') return
-  //     setTodos(previousTodos => {
-  //       return [...previousTodos, { id: uuidv4(), name: name, completed: false }]
-  //     })
-  //     todoNameRef.current.value = null
-  //   }
-
-  //   function handleClearTodos() {
-  //     const newTodos = todos.filter(todo => !todo.complete)
-  //     setTodos(newTodos)
-  //   }
+  // //to save the todos
+  // useEffect(() => {
+  //   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  // }, [todos])
 
   return (
-    //Fragment (to return more than one thing)
     <>
       <div className="todo-app">
-        <TodoForm />
-        {/* <TodoList todos={todos} toggleTodo={toggleTodo} /> */}
-        {/* <input ref={todoNameRef} type='text' /> */}
-        {/* <button onClick={handleClearTodos}>Clear Complete</button> */}
-        {/* <div>{todos.filter(todo => !todo.complete).length} left to do</div> */}
+        <TodoList />
       </div>
     </>
   )
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { v4 as uuidv4 } from 'uuid' //to generates random id
 
 export default function TodoForm(props) {
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState(props.edit ? props.edit.value : '')
+
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    inputRef.current.focus()
+  })
 
   const handleChange = e => {
     setInput(e.target.value)
@@ -10,27 +16,52 @@ export default function TodoForm(props) {
 
   const handleSubmit = e => {
     e.preventDefault()
-    // props.onSubmit({
-    //   id: uuidv4(),
-    //   text: input,
-    //   completed: false
-    // })
+    props.onSubmit({
+      id: uuidv4(),
+      text: input,
+      // completed: false
+    })
     setInput('')
   }
-
 
   return (
     <div>
       <form className='todo-form' onSubmit={handleSubmit}>
-        <input
-          type='text'
-          placeholder='Add a todo'
-          value={input}
-          name='text'
-          className='todo-input'
-          onChange={handleChange}
-        />
-        <button className='todo-button'>Add todo</button>
+        {props.edit ? (
+          <>
+            <input
+              type='text'
+              placeholder='Add a todo'
+              value={input}
+              name='text'
+              className='todo-input edit'
+              onChange={handleChange}
+              ref={inputRef}
+            />
+            <button className='todo-button edit'>Update</button>
+          </>
+        )
+          :
+          (
+            <>
+              <input
+                type='text'
+                placeholder='Add a todo'
+                value={input}
+                name='text'
+                className='todo-input'
+                onChange={handleChange}
+                ref={inputRef}
+              />
+              <button className='todo-button'>Add todo</button>
+            </>
+          )
+        }
+
+
+
+
+
       </form>
     </div>
   )
